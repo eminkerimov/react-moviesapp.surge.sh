b8502d03ae8a215c458003ff6f55dc050a59cbc7
eminkerimov <kerimov_emin1@mail.ru>
2021-11-02 05:11:32 +0400

    react-movie-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Movie from "./components/Movie";
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e86f2bbf1c8ee2160e90df236faed478";
+
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=e86f2bbf1c8ee2160e90df236faed478&query="
+
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const getMovies = (API) => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+  };
+
+  useEffect(() => {
+    getMovies(FEATURED_API)
+  }, []);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      getMovies(SEARCH_API + searchTerm);
+      setSearchTerm('');
+    }
+  }
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="search"
+            type="search"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
       </header>
+      <div className="movie-container">
+        {movies.length > 0 && movies.map(movie => (
+          <Movie key={movie.id} {...movie} />
+        ))}
+      </div>
     </div>
   );
 }
